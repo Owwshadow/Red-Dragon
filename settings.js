c9fd37170971d70455c5b5392e6e1e0341cf2f77
prabhasha2006 <107193785+Owwshadow@users.noreply.github.com>
2022-08-20 10:09:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -44,7 +44,7 @@ global.antibad = "false" // To remove bad word senders
 global.antispam = "true" // To remove or block spammers
 global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "false" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
-global.welcom_msg = "true"
+global.welcom_msg = "false"
 
 
 global.owner = ['+380944814219']
